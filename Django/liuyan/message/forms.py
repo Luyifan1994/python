cb49e6af911c50a355557ab4fbe6e88213ca283a
陆一帆 <ivan@luyifandeMacBook-Pro.local>
2018-02-25 00:42:46 +0800

    add some comment

--- Django/liuyan/message/forms.py
+++ Django/liuyan/message/forms.py
@@ -10,4 +10,5 @@ from django import forms
 
 class MessForm(forms.Form):
     name = forms.CharField(label=u'姓名', max_length=30)
-    content = forms.CharField(label=u'内容', widget=forms.TextInput({'style': 'width:200px;height:50px'}))
+    content = forms.CharField(label=u'内容', widget=forms.TextInput(
+        {'style': 'width:200px;height:50px'}))  # 使用widget可以conent对应的标签中添加属性
--- Django/liuyan/usersys/views.py
+++ Django/liuyan/usersys/views.py
@@ -9,6 +9,9 @@ from forms import RegistForm
 
 def register(request):
     # ('next', '')中的''表示默认值为''
+    # 从 get 或者 post 请求中获取 next 参数值
+    # get 请求中，next 通过 url 传递，即 /?next=value
+    # post 请求中，next 通过表单传递，即 <input type="hidden" name="next" value="{{ next }}"/>
     redirect_to = request.POST.get('next', request.GET.get('next', ''))
 
     if request.method == 'POST':
